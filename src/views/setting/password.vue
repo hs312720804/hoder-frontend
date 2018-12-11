3225ea5b91925689c2d2d2b97884c1a60b2ebb4c
liqi <liqi@coocaa.com>
2018-12-11 18:09:47 +0800

    个人信息已经密码修改页面渲染完成加部分功能验证功能

--- src/views/setting/password.vue
+++ src/views/setting/password.vue
@@ -1,14 +1,84 @@
 <template>
-    <div>
-        密码修改
+    <div class="modifyPassword">
+        <el-form :label-position="labelPosition" label-width="200px" :model="passwordModify" ref="passwordModify" :rules="rulesPwd">
+            <el-form-item label="原密码" prop="oldPassword">
+                <el-input v-model="passwordModify.oldPassword"></el-input>
+            </el-form-item>
+            <el-form-item label="新密码" prop="newPassword">
+                <el-input v-model="passwordModify.newPassword"></el-input>
+            </el-form-item>
+            <el-form-item label="确认新密码" prop="confirmPassword">
+                <el-input v-model="passwordModify.confirmPassword"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="savePassword">保存</el-button>
+            </el-form-item>
+        </el-form>
     </div>
 </template>
 <script>
 export default {
     data(){
+        var validateOldPass = (rule, value, callback) => {
+            if (value === ''){
+                callback(new Error('请输入原始密码'));
+            }else {
+                callback();
+            }
+        };
+        var validatePass = (rule, value, callback) => {
+            if (value === ''){
+                callback(new Error('请输入密码'));
+            }else {
+                if (this.passwordModify.newPassword !== ''){
+                    this.$refs.passwordModify.validateField('newPassword')
+                }
+                callback();
+            }
+        };
+        var validatePass2 = (rule, value, callback) => {
+            if (value === ''){
+                callback(new Error('请再次输入密码'));
+            }else if (value != this.passwordModify.newPassword) {
+                callback(new Error('两次输入密码不一致！'));
+            }else{
+                callback();
+            }
+        };
         return{
+            labelPosition: 'right',
+            passwordModify: {
+                oldPassword: '',
+                newPassword: '',
+                confirmPassword: ''
+            },
+            rulesPwd: {
+                oldPassword: [
+                    { validator :validateOldPass, trigger: 'blur'}
+                ],
+                newPassword: [
+                    { validator :validatePass, trigger: 'blur'}
+                ],
+                confirmPassword: [
+                    { validator :validatePass2, trigger: 'blur'}
+                ]
+            }
             
         }
+    },
+    methods: {
+        savePassword: function () {
+            this.$message('保存')
+        }
+
+    },
+    created: {
+
     }
 }
 </script>
+<style scoped>
+  .modifyPassword{
+      width: 500px;
+  }
+</style>
--- src/views/setting/personalInformation.vue
+++ src/views/setting/personalInformation.vue
@@ -1,14 +1,52 @@
 <template>
-    <div>
-        个人信息
+    <div class="personalInformation">
+        <el-form :label-position="labelPosition" label-width="200px" :model="personalInformation">
+            <el-form-item label="账号" prop="id">
+                {{personalInformation.id}}
+            </el-form-item>
+            <el-form-item label="归属机构" prop="officeName">
+                {{personalInformation.officeName}}
+            </el-form-item>
+            <el-form-item label="真实姓名" prop="username">
+                <el-input v-model="personalInformation.username" placeholder="如若修改请填写"></el-input>
+            </el-form-item>
+            <el-form-item label="手机" prop="mobile">
+                <el-input v-model="personalInformation.mobile" placeholder="如若修改请填写"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱" prop="email">
+                <el-input v-model="personalInformation.email" placeholder="如若修改请填写"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="saveInformation">保存</el-button>
+            </el-form-item>
+        </el-form>
     </div>
 </template>
 <script>
 export default {
     data(){
         return{
+            labelPosition: 'right',
+            personalInformation: {
+                id: 'liqi@coocaa.com',
+                officeName: '研发部',
+                username: '',
+                mobile: '',
+                email: '',
+
+            }
             
+        }
+    },
+    methods: {
+        saveInformation: function () {
+            alert("保存")
         }
     }
 }
 </script>
+<style scoped>
+    .personalInformation{
+        width: 500px;
+    }
+</style>
