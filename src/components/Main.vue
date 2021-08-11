0ba02709d49d64bd1976e7c6889d026b5003c6fc
huangshan@coocaa.com <312720804>
2021-08-11 17:59:59 +0800

    feat(Main): 添加帮助文档

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -72,6 +72,7 @@
         ></el-button>
         <c-breadcrumb class="breadcrumb" :items="breadcrumb"/>
         <div class="user-info">
+          <el-button type="text" @click="handelGoToHelp">帮助文档</el-button>
           <el-dropdown trigger="hover">
             <el-badge :value="unReadMessage" class="item">
               <i class="el-icon-bell"></i>
@@ -251,165 +252,170 @@
             }
         },
         methods: {
-            setCurrentTags(val) {
-              this.currentTags = val
-            },
+          handelGoToHelp () {
+            const url = 'http://wiki.skyoss.com/pages/viewpage.action?pageId=49470290'
+            window.open(url, '_blank');
+          },
 
-            handleRouteChange () {
-              if (this.$route.name === 'oneTouchDrop') {
-                  this.activeRouteName = 'strategyList'
-              } else if (this.$route.name === 'specialTag') {
-                  this.activeRouteName = 'labelSquare'
-              } else {
-                  this.activeRouteName = this.$route.name
-              }
-              
-              // 打开的菜单页面可以缓存，未打开的菜单页面不能缓存
-              const meta = this.$route.meta;
+          setCurrentTags (val) {
+            this.currentTags = val
+          },
 
-              const obj = this.currentTags.find(item => {
-                return item.meta.tagId === meta.tagId
-              })
-
-              if (obj) { // 菜单在已打开的菜单中存在，则缓存
-                this.isCache = true // 缓存
-              } else {
-                this.isCache = false // 不缓存
-              }
-
-            },
-            getRouter (url) {
-                const name = this.routerMap[url] + 'AA'
-                this.noCacheMenu.push(name)
-                this.$router.push({name: this.routerMap[url]})
-                this.$nextTick(() => {
-                    this.noCacheMenu = []
-                })
-            },
-            handleDropdownCommand (command) {
-                if (command === "logout") {
-                    this.$logout().then(() => {
-                      this.$router.push({ name: "login" });
-                    });
-                }
-            },
-            toggleMenu () {
-                const isCollapseMenu = !this.isCollapseMenu;
-                this.$appState.$set("isCollapseMenu", isCollapseMenu);
-                this.isCollapseMenu = isCollapseMenu;
-            },
-            saveTags () {
-                const tags = {tags: this.$refs.tag.tags, userName: this.$appState.user.name};
-                this.$appState.$set("tags", tags);
-            },
-            setMetaTitle () {
-                const routes = this.$router.options.routes
-                const findRouteByName = (name, route) => {
-                    if (Array.isArray(route)) {
-                        let length = route.length
-                        while(--length >= 0) {
-                            const found = findRouteByName(name, route[length])
-                            if (found) {
-                                return found
-                            }
-                        }
-                    } else {
-                        if (route.name === name) {
-                            return route
-                        } else if (route.children) {
-                            return findRouteByName(name, route.children)
-                        }
-                    }
-                }
-                const menus = this.$appState.menus
-                const findMenuByUrl = (url, menu) => {
-                    if (Array.isArray(menu)) {
-                        let length =  menu.length
-                        while(--length >= 0) {
-                            const found = findMenuByUrl(url, menu[length])
-                            if (found) {
-                                return found
-                            }
-                        }
-                    } else {
-                        if (menu.url === url) {
-                            return menu
-                        } else if (menu.child) {
-                            return findMenuByUrl(url, menu.child)
-                        }
-                    }
-                }
-                const routerMap = this.routerMap
-                Object.keys(routerMap).forEach((url) => {
-                    const menu = findMenuByUrl(url, menus)
-                    const route = findRouteByName(routerMap[url], routes)
-                    if (route && route.meta && menu) {
-                        route.meta.title = menu.name
-                    }
-
-                })
-            },
-            handleLogoClick() {
-                this.$router.push({
-                    path: '/statisticsHomePage'
-                })
-            },
-            handleClick(tab) {
-                this.getNoticeMessages(this.messageTypeEnum[tab.name])
-            },
-            getNoticeMessages (type) {
-                const noticeType = type
-                this.$service.getNoticeHeaderList({noticeType}).then((data) => {
-                    let interfaceData = data['消息列表']
-                    let statusData = data['消息状态']
-                    this.unReadMessage = data['未读数量'] > 0 ? data['未读数量'] : undefined
-                    interfaceData.forEach((item,index)=> {
-                        item.noticeStatus = statusData[index]
-                    })
-                    let dataList = interfaceData
-                    if (type === 1) {
-                        if (dataList.length > 5) {
-                            this.showMoreUpdate = true
-                            // 只取前五条
-                            this.updateMessage = dataList.slice(0, 5)
-                        } else {
-                            this.updateMessage = dataList
-                        }
-                    }
-                    else {
-                        if (dataList.length > 5) {
-                            this.showMoreSystem = true
-                            this.systemMessage = dataList.slice(0, 5)
-                        } else {
-                            this.systemMessage = dataList
-                        }
-                    }
-                })
-            },
-            handleSeeAllMessage () {
-                this.$router.push({name: 'notice'})
-            },
-            handleReadMessage (noticeId) {
-                this.$router.push({
-                    name: 'notice',
-                    query: {noticeId: noticeId, mode: 'read'}
-                })
-            },
-            scrollMenuIntoView () {
-                setTimeout(() => {
-                    const $activeSubMenu = document.querySelector('.el-submenu.is-active')
-                    const $activeMenu = document.querySelector('.el-menu-item.is-active')
-                    if ($activeMenu) {
-                        $activeMenu.scrollIntoViewIfNeeded()
-                    }
-                    if ($activeSubMenu) {
-                        $activeSubMenu.scrollIntoViewIfNeeded()
-                    }
-                }, 1000)
+          handleRouteChange () {
+            if (this.$route.name === 'oneTouchDrop') {
+                this.activeRouteName = 'strategyList'
+            } else if (this.$route.name === 'specialTag') {
+                this.activeRouteName = 'labelSquare'
+            } else {
+                this.activeRouteName = this.$route.name
             }
-            // handleDropDownChange() {
-            //     this.getNoticeMessages(this.messageTypeEnum[this.activeName])
-            // }
+            
+            // 打开的菜单页面可以缓存，未打开的菜单页面不能缓存
+            const meta = this.$route.meta;
+
+            const obj = this.currentTags.find(item => {
+              return item.meta.tagId === meta.tagId
+            })
+
+            if (obj) { // 菜单在已打开的菜单中存在，则缓存
+              this.isCache = true // 缓存
+            } else {
+              this.isCache = false // 不缓存
+            }
+
+          },
+          getRouter (url) {
+              const name = this.routerMap[url] + 'AA'
+              this.noCacheMenu.push(name)
+              this.$router.push({name: this.routerMap[url]})
+              this.$nextTick(() => {
+                  this.noCacheMenu = []
+              })
+          },
+          handleDropdownCommand (command) {
+              if (command === "logout") {
+                  this.$logout().then(() => {
+                    this.$router.push({ name: "login" });
+                  });
+              }
+          },
+          toggleMenu () {
+              const isCollapseMenu = !this.isCollapseMenu;
+              this.$appState.$set("isCollapseMenu", isCollapseMenu);
+              this.isCollapseMenu = isCollapseMenu;
+          },
+          saveTags () {
+              const tags = {tags: this.$refs.tag.tags, userName: this.$appState.user.name};
+              this.$appState.$set("tags", tags);
+          },
+          setMetaTitle () {
+              const routes = this.$router.options.routes
+              const findRouteByName = (name, route) => {
+                  if (Array.isArray(route)) {
+                      let length = route.length
+                      while(--length >= 0) {
+                          const found = findRouteByName(name, route[length])
+                          if (found) {
+                              return found
+                          }
+                      }
+                  } else {
+                      if (route.name === name) {
+                          return route
+                      } else if (route.children) {
+                          return findRouteByName(name, route.children)
+                      }
+                  }
+              }
+              const menus = this.$appState.menus
+              const findMenuByUrl = (url, menu) => {
+                  if (Array.isArray(menu)) {
+                      let length =  menu.length
+                      while(--length >= 0) {
+                          const found = findMenuByUrl(url, menu[length])
+                          if (found) {
+                              return found
+                          }
+                      }
+                  } else {
+                      if (menu.url === url) {
+                          return menu
+                      } else if (menu.child) {
+                          return findMenuByUrl(url, menu.child)
+                      }
+                  }
+              }
+              const routerMap = this.routerMap
+              Object.keys(routerMap).forEach((url) => {
+                  const menu = findMenuByUrl(url, menus)
+                  const route = findRouteByName(routerMap[url], routes)
+                  if (route && route.meta && menu) {
+                      route.meta.title = menu.name
+                  }
+
+              })
+          },
+          handleLogoClick() {
+              this.$router.push({
+                  path: '/statisticsHomePage'
+              })
+          },
+          handleClick(tab) {
+              this.getNoticeMessages(this.messageTypeEnum[tab.name])
+          },
+          getNoticeMessages (type) {
+              const noticeType = type
+              this.$service.getNoticeHeaderList({noticeType}).then((data) => {
+                  let interfaceData = data['消息列表']
+                  let statusData = data['消息状态']
+                  this.unReadMessage = data['未读数量'] > 0 ? data['未读数量'] : undefined
+                  interfaceData.forEach((item,index)=> {
+                      item.noticeStatus = statusData[index]
+                  })
+                  let dataList = interfaceData
+                  if (type === 1) {
+                      if (dataList.length > 5) {
+                          this.showMoreUpdate = true
+                          // 只取前五条
+                          this.updateMessage = dataList.slice(0, 5)
+                      } else {
+                          this.updateMessage = dataList
+                      }
+                  }
+                  else {
+                      if (dataList.length > 5) {
+                          this.showMoreSystem = true
+                          this.systemMessage = dataList.slice(0, 5)
+                      } else {
+                          this.systemMessage = dataList
+                      }
+                  }
+              })
+          },
+          handleSeeAllMessage () {
+              this.$router.push({name: 'notice'})
+          },
+          handleReadMessage (noticeId) {
+              this.$router.push({
+                  name: 'notice',
+                  query: {noticeId: noticeId, mode: 'read'}
+              })
+          },
+          scrollMenuIntoView () {
+              setTimeout(() => {
+                  const $activeSubMenu = document.querySelector('.el-submenu.is-active')
+                  const $activeMenu = document.querySelector('.el-menu-item.is-active')
+                  if ($activeMenu) {
+                      $activeMenu.scrollIntoViewIfNeeded()
+                  }
+                  if ($activeSubMenu) {
+                      $activeSubMenu.scrollIntoViewIfNeeded()
+                  }
+              }, 1000)
+          }
+          // handleDropDownChange() {
+          //     this.getNoticeMessages(this.messageTypeEnum[this.activeName])
+          // }
         },
         created() {
             this.isCollapseMenu = !!this.$appState.$get("isCollapseMenu");
