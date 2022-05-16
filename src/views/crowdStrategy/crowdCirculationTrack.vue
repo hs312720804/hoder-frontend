af95cd9404ddea635fa33b65a2e2520bfb97ea2e
huangshan <huangshan@coocaa.com>
2022-05-16 20:30:47 +0800

    feat(crowdCirculationTrack): 新增链路图功能

--- src/views/crowdStrategy/crowdCirculationTrack.vue
+++ src/views/crowdStrategy/crowdCirculationTrack.vue
@@ -28,26 +28,16 @@
     </el-form>
     <div>
 
-      <div v-if="list.length > 0">
-        <div>
-          命中情况：
-        </div>
-        <div class="result">
-          <div v-for="(item, index) in list" :key="index" class="result-item">
-            <div>
-              <span style="display: inline-block; width: 150px;">{{ item.mac }}</span>
-              <span>命中时间：{{ item.hitDate }}</span>
-            </div>
-          </div>
-        </div>
-
-      </div>
-      <div v-else>{{ noneText }}</div>
+      <el-empty v-if="list.length === 0" description="暂无数据"></el-empty>
+      <div :id="pageId" class="graph-container" style="position: relative;"></div>
+      <!-- <Flow></Flow> -->
     </div>
   </div>
 </template>
 
 <script>
+import G6 from '@antv/g6/build/g6'
+import Flow from '@antvGraph/components/Flow'
 export default {
   name: 'crowdCirculationTrackAA',
   data () {
@@ -76,10 +66,210 @@ export default {
         }
       },
       list: [],
-      noneText: ''
+      noneText: '暂无数据',
+      pageId: 'graph-container',
+      graph: null,
+      dialogVisible: false,
+      crowdId: undefined, // 小人群ID
+      allCrowdRule: [],
+      len: 118 // 模拟数据个数
     }
   },
+  components: {
+    Flow
+  },
+  created () {
+    // 扩展内置的 【circle】 shape
+    const defaultNodeConf = {
+      afterDraw (cfg, group) {
+        // console.log('cfg===', cfg)
+        // console.log('group===', group)
+        const height = parseInt(cfg.size)
+        group.addShape('text', {
+          // attrs: {
+          //   x: 0,
+          //   y: height / 2 + 20,
+          //   // cursor: 'pointer',
+          //   textAlign: 'center',
+          //   text: `${cfg.id}`,
+          //   fill: 'red'
+          // }
+          attrs: {
+            x: 0,
+            y: height / 2 + 20,
+            text: `${cfg.date}`,
+            // fill: '#87adfb',
+            fill: '#87adfb',
+            fontWeight: 400,
+            // shadowOffsetX: 10,
+            // shadowOffsetY: 10,
+            // shadowColor: '#999',
+            // shadowBlur: 10,
+            textAlign: 'center',
+            fontSize: 12
+          }
+        })
+      }
+    }
+    G6.registerNode(
+      'circle-node',
+      defaultNodeConf,
+      // 注意这里继承了 'single-shape'
+      'circle'
+    )
+  },
+  mounted () {
+    // this.init()
+  },
   methods: {
+    init () {
+      const width = document.getElementById('graph-container').scrollWidth
+      // const height = document.getElementById('graph-container').scrollHeight || 800
+      const height = this.list.length < 20 ? 400 : this.list.length / 15 * 250
+      // const height = this.len < 20 ? 400 : this.len / 15 * 250
+      // const height = 150 * 16
+
+      this.graph = new G6.Graph({
+        container: 'graph-container',
+        width,
+        height,
+        layout: {
+          type: 'grid',
+          begin: [ 20, 20 ],
+          width: width - 20,
+          height: height - 20,
+          sortBy: 'cluster'
+        },
+        defaultNode: {
+
+          shape: 'circle-node',
+          size: 70,
+          style: {
+            fill: '#DEE9FF',
+            stroke: '#5B8FF9'
+          },
+          nodeStateStyles: {
+            // 鼠标hover状态下的配置
+            hover: {
+              fillOpacity: 0.8
+            },
+            // 选中节点状态下的配置
+            selected: {
+              lineWidth: 5
+            }
+          }
+          // labelCfg: {
+          //   position: 'bottom',
+          //   offset: 10,
+          //   style: {
+          //   // ... 文本样式的配置
+          //   }
+          // }
+
+        },
+        defaultEdge: {
+          // shape: 'polyline',
+          style: {
+            radius: 20,
+            offset: 30,
+            endArrow: true,
+            stroke: '#F6BD16'
+          }
+        },
+        modes: {
+          // 支持的 behavior
+          default: [ 'drag-node' ]
+        },
+        nodeStateStyles: {
+          // 鼠标hover状态下的配置
+          hover: {
+            fillOpacity: 0.8
+          },
+          // 选中节点状态下的配置
+          selected: {
+            lineWidth: 5
+          }
+        }
+      })
+
+      // graph.read(data)
+      this.readOrClearData()
+
+      this.graph.on('node:mouseenter', evt => {
+        const { item } = evt
+        this.graph.setItemState(item, 'hover', true)
+      })
+      this.graph.on('node:mouseleave', evt => {
+        const { item } = evt
+        this.graph.setItemState(item, 'hover', false)
+      })
+      // this.graph.on('node:click', evt => {
+      //   const { item } = evt
+      //   this.graph.setItemState(item, 'selected', true)
+      // })
+    },
+    // 重构成图表所需数据格式
+    reconstructData  (data) {
+      console.log('aaaaaa=======', data)
+      // --------------构造所需数据结构--------------------
+      const nodes = []
+      const edges = []
+
+      let len = data.length
+      data.map(item => {
+        nodes.push({
+          id: item.crowdId,
+          label: `人群${item.crowdId}`,
+          cluster: len--,
+          date: item.hitDate
+        })
+      })
+
+      // -- 模拟数据 --
+      // let num = this.len
+      // for (let i = 1; i < this.len; i++) {
+      //   nodes.push({
+      //     id: i,
+      //     label: `人群${i}`,
+      //     cluster: num--,
+      //     date: '2022-05-16 16:35:30'
+      //   })
+      // }
+      // --模拟数据 end--
+      console.log('节点============', nodes)
+
+      nodes.reduce((current, item) => {
+        edges.push({
+          source: current.id.toString(),
+          target: item.id.toString()
+        })
+        return item
+      }, nodes[0])
+
+      edges.shift()
+      console.log('箭头============', edges)
+
+      return {
+        nodes,
+        edges
+      }
+      // -------------- 构造所需数据结构- end-------------------
+    },
+    // 渲染 或者 清空图表
+    readOrClearData () {
+      // 清空图表
+      if (this.list.length === 0) {
+        this.graph && this.graph.clear()
+        this.graph.read({})
+        return
+      }
+      // 渲染图表
+      let data = this.reconstructData(this.list)
+      if (data && data.nodes.length > 0) {
+        // read 方法的功能相当于 data 和 render 方法的结合。
+        this.graph.read(data)
+      }
+    },
     getFilter () {
       const filter = {
         policyId: this.form.policyId,
@@ -91,6 +281,12 @@ export default {
       return filter
     },
     handleSearch () {
+      // 清空数据、清空图表
+      this.list = []
+      console.log('this.graph====', this.graph)
+      // this.graph && this.graph.clear() && this.graph.read({})
+      this.graph && this.graph._cfg && this.graph.destroy() // 若有图表，则销毁
+
       this.$refs.form.validate(valid => {
         if (valid) {
           const filter = this.getFilter()
@@ -98,12 +294,10 @@ export default {
           this.$service.dynamicCrowdHitLink(filter).then(data => {
             console.log('data=====>', data)
             // 链路数据
-            this.list = data
+            this.list = data || []
 
-            // this.list = data.pageInfo.list || '暂无数据'
-            // if (this.list === undefined || this.list.length === 0) {
-            //   this.noneText = '该人群不存在'
-            // }
+            // 生成链路图
+            this.init()
           })
         }
       })
@@ -123,11 +317,155 @@ export default {
     //   }
     //   this.dateList = a
     //   this.form.date = a[1]
-    // }
-  },
-  created () {
-    // this.setDateData()
+    // },
+    // initaaa () {
+    //   G6.registerEdge('line-arrow', {
+    //     options: {
+    //       style: {
+    //         stroke: '#ccc'
+    //       }
+    //     },
+    //     draw: function draw (cfg, group) {
+    //       const startPoint = cfg.startPoint
+    //       const endPoint = cfg.endPoint
+
+    //       const stroke = cfg.style && cfg.style.stroke || this.options.style.stroke
+
+    //       const keyShape = group.addShape('path', {
+    //         attrs: {
+    //           path: [[ 'M', startPoint.x, startPoint.y ], [ 'L', endPoint.x / 3 + 2 / 3 * startPoint.x, startPoint.y ], [ 'L', endPoint.x / 3 + 2 / 3 * startPoint.x, endPoint.y ], [ 'L', endPoint.x, endPoint.y ]],
+    //           stroke,
+    //           lineWidth: 1,
+    //           // startArrow: {
+    //           //   path: 'M 6,0 L -6,-6 L -3,0 L -6,6 Z',
+    //           //   d: 6
+    //           // },
+    //           endArrow: {
+    //             path: 'M 6,0 L -6,-6 L -3,0 L -6,6 Z',
+    //             d: 6
+    //           },
+    //           className: 'edge-shape'
+    //         }
+    //       })
+    //       return keyShape
+    //     }
+    //   })
+
+    //   const data = {
+    //     nodes: [
+    //       {
+    //         id: '1',
+    //         label: 'node1',
+    //         // x: 150,
+    //         // y: 100,
+    //         size: 40
+    //         // anchorPoints: [[ 1, 0.5 ], [ 1, 0 ]]
+    //       }, {
+    //         id: '2',
+    //         label: 'node2',
+    //         // x: 150,
+    //         // y: 100,
+    //         size: 40
+    //         // anchorPoints: [[ 1, 0.5 ], [ 1, 0 ]]
+    //       }, {
+    //         id: '3',
+    //         label: 'node3',
+    //         // x: 150,
+    //         // y: 100,
+    //         size: 40
+    //         // anchorPoints: [[ 1, 0.5 ], [ 1, 0 ]]
+    //       }, {
+    //         id: '4',
+    //         label: 'node4',
+    //         // x: 150,
+    //         // y: 100,
+    //         size: 40
+    //         // anchorPoints: [[ 1, 0.5 ], [ 1, 0 ]]
+    //       }, {
+    //         id: '7',
+    //         label: 'node7',
+    //         // x: 150,
+    //         // y: 100,
+    //         size: 40
+    //         // anchorPoints: [[ 1, 0.5 ], [ 1, 0 ]]
+    //       }, {
+    //         id: '8',
+    //         label: 'node8',
+    //         // x: 300,
+    //         // y: 200,
+    //         size: 40
+    //         // anchorPoints: [[ 0, 0.5 ], [ 0, 1 ]]
+    //       }],
+    //     edges: [
+    //       {
+    //         source: '1',
+    //         target: '2'
+    //         // sourceAnchor: 0,
+    //         // targetAnchor: 0
+    //       }, {
+    //         source: '2',
+    //         target: '3'
+    //         // sourceAnchor: 0,
+    //         // targetAnchor: 0
+    //       },
+    //       {
+    //         source: '3',
+    //         target: '4'
+    //         // sourceAnchor: 0,
+    //         // targetAnchor: 0
+    //       }]
+    //   }
+
+    //   const width = document.getElementById('graph-container').scrollWidth
+    //   const height = document.getElementById('graph-container').scrollHeight || 500
+    //   const graph = new G6.Graph({
+    //     container: 'graph-container',
+    //     width,
+    //     height,
+    //     layout: {
+    //       type: 'grid',
+    //       begin: [ 20, 20 ]
+    //     },
+    //     modes: {
+    //       // 支持的 behavior
+    //       default: [ 'drag-node', 'drag-canvas' ]
+    //     },
+    //     defaultNode: {
+    //       shape: 'circle',
+    //       style: {
+    //         fill: '#DEE9FF',
+    //         stroke: '#5B8FF9'
+    //       }
+    //       // linkPoints: {
+    //       //   left: true,
+    //       //   right: true,
+    //       //   fill: '#fff',
+    //       //   stroke: '#1890FF',
+    //       //   size: 3
+    //       // }
+    //     },
+    //     defaultEdge: {
+    //       shape: 'polyline',
+    //       style: {
+    //         radius: 10,
+    //         offset: 30,
+    //         endArrow: true,
+    //         stroke: '#F6BD16'
+    //       }
+    //     }
+    //     // defaultEdge: {
+    //     //   shape: 'line-arrow',
+    //     //   style: {
+    //     //     stroke: '#F6BD16'
+    //     //   }
+    //     // }
+    //   })
+
+    //   graph.data(data)
+    //   graph.render()
+    // },
   }
+
 }
 </script>
 
