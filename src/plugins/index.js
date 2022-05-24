6b3bab006c123d693a976782a3e03ee63aa97605
huangshan <huangshan@coocaa.com>
2022-05-24 16:47:11 +0800

    refactor(loadmore): 给指令 loadmore 添加底部余量

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -32,7 +32,7 @@ Vue.directive('loadmore', {
     // 获取element-ui定义好的scroll盒子
     const SELECTWRAP_DOM = el.querySelector('.el-select-dropdown .el-select-dropdown__wrap')
     SELECTWRAP_DOM.addEventListener('scroll', function () {
-      const CONDITION = this.scrollHeight - this.scrollTop <= this.clientHeight
+      const CONDITION = this.scrollHeight - this.scrollTop - 20 <= this.clientHeight
       if (CONDITION) {
         binding.value.methord(binding.value.params)
       }
