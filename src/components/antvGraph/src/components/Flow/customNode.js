a8b9de72bfe3ff74e4e1626fc310ab912525c35f
huangshan <huangshan@coocaa.com>
2022-07-29 15:15:34 +0800

    refactor(customNode.js; crowdList) : 拓扑图中添加 ID 展示

--- src/components/antvGraph/src/components/Flow/customNode.js
+++ src/components/antvGraph/src/components/Flow/customNode.js
@@ -128,16 +128,16 @@ const customNode = {
           })
           weightText.set('className', 'changeWeight')
         }
-        // group.addShape('text', {
-        //   attrs: {
-        //     x: -(width / 2) + 90,
-        //     y: offsetY + height + 20,
-        //     cursor: 'pointer',
-        //     label: 'aaaaa',
-        //     text: `id:  ${cfg.id}`,
-        //     fill: color
-        //   }
-        // })
+        group.addShape('text', {
+          attrs: {
+            x: -(width / 2) + 90,
+            y: offsetY + height + 20,
+            cursor: 'pointer',
+            label: 'aaaaa',
+            text: `id:  ${cfg.id}`,
+            fill: color
+          }
+        })
         console.log('cfg===', cfg)
         // // 流转条件 设置className属性
         // const innerCircle2 = group.addShape('rect', {
