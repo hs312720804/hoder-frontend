ef9f07be5602eabf26c3819e22622580acd26210
huangshan <huangshan@coocaa.com>
2022-08-10 20:17:47 +0800

    refactor(plugins): 修改处理数字的方法cc_format_number，兼容有小数的情况

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -40,18 +40,57 @@ Vue.directive('loadmore', {
   }
 })
 
-Vue.prototype.cc_format_number = function (n) {
+// Vue.prototype.cc_format_number = function (n) {
+//   // 如果不是字符或者数字，则直接返回
+//   if (typeof n !== 'string' && typeof n !== 'number') {
+//     return n
+//   }
+//   if (typeof n !== 'string') {
+//     n = n.toString()
+//   }
+//   var len = n.length
+//   if (len <= 3) { return n }
+//   var r = len % 3
+//   const start = n.slice(0, r)
+//   const end = n.slice(r).match(/\d{3}/g).join(',')
+//   return r > 0 ? start + ',' + end : end
+// }
+
+Vue.prototype.cc_format_number = function (number) {
   // 如果不是字符或者数字，则直接返回
-  if (typeof n !== 'string' && typeof n !== 'number') {
-    return n
+  if (typeof number !== 'string' && typeof number !== 'number') {
+    return number
   }
-  if (typeof n !== 'string') {
-    n = n.toString()
+  if (typeof number !== 'string') {
+    number = number.toString()
   }
-  var len = n.length
-  if (len <= 3) { return n }
-  var r = len % 3
-  const start = n.slice(0, r)
-  const end = n.slice(r).match(/\d{3}/g).join(',')
-  return r > 0 ? start + ',' + end : end
+  // 将number转为String类型
+  // let num = number + ''
+  //  将一个浮点数分为一个数组，第一个元素是整数部分，第二个元素是小数部分
+  let numArr = number.split('.')
+  // 使用结构赋值
+  let [int, dotNum] = numArr
+  // [...int]相当于int.split('')
+  let revint = [...int].reverse()
+  let len = revint.length
+  let res = []
+  for (let i = 0; i < len; i++) {
+  // 从个位开始遍历
+    if ((i + 1) % 3 === 0 && i !== len - 1) {
+      res.push(`,${revint[i]}`)
+    } else {
+      res.push(revint[i])
+    }
+  }
+  // console.log('res------------>', res)
+  // 判断是否有小数
+  if (dotNum) {
+    res.reverse().push('.', ...dotNum)
+    // 将结果转为Number类型
+    // console.log('小数的------------>',res)
+    return res.join('')
+  } else {
+    // console.log('整数的------------>',res)
+    return res.reverse().join('')
+  }
 }
