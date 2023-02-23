68a5620673e982182c14a863cf52324a8cb16842
huangshan <huangshan@coocaa.com>
2023-02-23 11:19:05 +0800

    feat(fetch): 解决登录过期之后报错的问题：登录过期之后直接跳转回登录页

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -2,6 +2,7 @@ import qs from 'qs'
 import axios from 'axios'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
+import { Message } from 'element-ui'
 
 // 响应拦截器, 401 状态码时，跳转至登录页
 axios.interceptors.response.use((response) => {
@@ -11,11 +12,19 @@ axios.interceptors.response.use((response) => {
   if (error.response && error.response.status === 401) {
     // window.location = '/login'
     // window.location.reload()
+    // // location.href = location.origin + location.pathname + '#/login'
+    // const lo = location.origin + location.pathname + '#/login'
+    // console.log('router--->', router)
+    // console.log('location--->', lo)
+    // // window.location = lo
+    // location.href = lo
+    // // router.replace('/login')
     console.log('token 失效啦')
+    Message.info({ type: 'danger', message: '身份已经过期，请重新登录' })
+    // router.replace('/login').catch(() => {})
     // return new Promise(function () {}) // 空的Promise对象，没有机会执行catch，进而不做错误提示了
-  } else {
-    return Promise.reject(error)
   }
+  return Promise.reject(error)
 })
 
 export default function fetch ({
